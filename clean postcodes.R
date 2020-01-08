01460e4d8c7d040b430a93bb54976a28e8ab677a
Shhh <shanwilkinson2@users.noreply.github.com>
2020-01-08 15:16:00 +0000

    clean some errors
    change to caps, trim, get rid of double spaces, check if valid now

--- clean postcodes.R
+++ clean postcodes.R
@@ -1,13 +1,30 @@
 library(dplyr)
 library(stringr)
+# library(purrr)
 
 # create sample postcodes 
-pcodes <- c("M1 1AF", "M46 0AA", "BL1 1RU", "SY11 2PR", "W1A 1AA", "SW1A 1AA", "M46 OAA", "BL1", "BOLTON", "bl! 1R", "bl1 1ru", "BL1  1RU")
+  pcodes <- c("M1 1AF", "M46 0AA", "BL1 1RU", "bl1 1ru", " bl! 1ru" , "SY11 2PR", "  W1A 1AA", "SW1A 1AA", "M46 OAA", "BL1", "BOLTON", "bl! 1R", "bl1  1ru", "BL1  1RU")
 
-#"^[A-Z]{1,2}\d[A-Z\d]? ?\d[A-Z]{2}$"
+# create a dataframe to hold input postcode, output postcode, whether postcode is valid as it is
+  output <- data.frame(input_pcode = as.character(pcodes), 
+                      valid = str_detect(pcodes, "^[A-Z]{1,2}\\d[A-Z\\d]? ?\\d[A-Z]{2}$"),
+                      stringsAsFactors = FALSE)
 
-str_detect(pcodes, "^[A-Z]{1,2}\\d[A-Z\\d]? ?\\d[A-Z]{2}$")
+# if postcode is valid as it is, copy input postcode to output postcode  
+  output$output_pcode <- ifelse(output$valid == TRUE, yes = output$input_pcode, no = "")
 
+# want to keep the column as initial validity so can see how much time it's saved. 
+  # create a new valid vector as cleaning is done
+  output$final_valid <- output$valid
+  
+# trim trailing & leading whitespace, convert to uppercase, replace double spaces, check if valid now   
+  output$output_pcode <- ifelse(output$valid == FALSE, 
+                                yes = output$input_pcode %>%
+                                  str_trim() %>%
+                                  str_to_upper() %>%
+                                  str_replace_all("  ", " "), 
+                                no = output$output_pcode)
+  output$final_valid <- ifelse(str_detect(output$output_pcode, "^[A-Z]{1,2}\\d[A-Z\\d]? ?\\d[A-Z]{2}$"), yes = TRUE, no = FALSE)
 
   
 # This doesn't cover overseas territories and only enforces the format, NOT the existence of different areas. It is based on the following rules:
